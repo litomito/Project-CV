b87eb5bcc7ad587f824fa3917a4a0855d27b8966
litomito <linustoma11@hotmail.com>
2023-06-25 15:29:16 +0200

    The code

---
+++ script.js
@@ -0,0 +1,141 @@
+let popup = document.getElementById("popup1");
+
+
+function openPopup(){
+    popup.classList.add("open-popup1")
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("active")
+    var kon = document.getElementById("kon1");
+    kon.classList.toggle("active")
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup1")
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("active")
+    var kon = document.getElementById("kon1");
+    kon.classList.toggle("active")
+};
+
+
+let popup2 = document.getElementById("popup2");
+
+function openPopup2(){
+    popup2.classList.add("open-popup2")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var per = document.getElementById("pers");
+    per.classList.toggle("active")
+}
+
+function closePopup2(){
+    popup2.classList.remove("open-popup2")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var per = document.getElementById("pers");
+    per.classList.toggle("active")
+};
+
+
+let popup3 = document.getElementById("popup3");
+
+function openPopup3(){
+    popup3.classList.add("open-popup3")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var spr1 = document.getElementById("spr");
+    spr1.classList.toggle("active")
+}
+
+function closePopup3(){
+    popup3.classList.remove("open-popup3")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var spr1 = document.getElementById("spr");
+    spr1.classList.toggle("active")
+};
+
+
+let popup5 = document.getElementById("popup5");
+
+function openPopup5(){
+    popup5.classList.add("open-popup5")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var ommig = document.getElementById("om-mig")
+    ommig.classList.toggle("active")
+}
+
+function closePopup5(){
+    popup5.classList.remove("open-popup5")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var ommig = document.getElementById("om-mig")
+    ommig.classList.toggle("active")
+};
+
+let popup6 = document.getElementById("popup6");
+
+function openPopup6(){
+    popup6.classList.add("open-popup6")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var imge = document.getElementById("imge")
+    imge.classList.toggle("active")
+};
+
+function closePopup6(){
+    popup6.classList.remove("open-popup6")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var imge = document.getElementById("imge")
+    imge.classList.toggle("active")
+};
+
+function openPopup7(){
+    popup7.classList.add("open-popup7")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var arbetsbtn = document.getElementById("arbets-btn")
+    arbetsbtn.classList.toggle("active")
+}
+
+function closePopup7(){
+    popup7.classList.remove("open-popup7")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var arbetsbtn = document.getElementById("arbets-btn")
+    arbetsbtn.classList.toggle("active")
+};
+
+function openPopup8(){
+    popup8.classList.add("open-popup8")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var utbildbtn = document.getElementById("utbild-btn")
+    utbildbtn.classList.toggle("active")
+}
+
+function closePopup8(){
+    popup8.classList.remove("open-popup8")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var utbildbtn = document.getElementById("utbild-btn")
+    utbildbtn.classList.toggle("active")
+};
+
+function openPopup9(){
+    popup9.classList.add("open-popup9")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var itkunbtn = document.getElementById("itkun-btn")
+    itkunbtn.classList.toggle("active")
+}
+
+function closePopup9(){
+    popup9.classList.remove("open-popup9")
+    var blur = document.getElementById("blur")
+    blur.classList.toggle("active")
+    var itkunbtn = document.getElementById("itkun-btn")
+    itkunbtn.classList.toggle("active")
+}
